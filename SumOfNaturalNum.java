6416cd48c8b258b43b69e651ba125f50a067ad9d
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-28 00:58:42 +0530

    Add files via upload

---
+++ SumOfNaturalNum.java
@@ -0,0 +1,13 @@
+//Java Program to Calculate Sum of Natural Numbers
+public class SumOfNaturalNum
+{  
+public static void main(String[] args)   
+{  
+int i, num = 5, sum = 0;    
+for(i = 1; i <= num; ++i)  
+{   
+sum = sum + i;  
+}  
+System.out.println("Sum of First 5 Natural Numbers is = " + sum);  
+}  
+}  
