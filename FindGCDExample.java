7eb950dfec3a54650a432cc9b1f26b2d874a98cd
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-28 01:13:29 +0530

    Add files via upload

---
+++ FindGCDExample.java
@@ -0,0 +1,16 @@
+//Java Program to Find GCD
+
+public class FindGCDExample 
+{  
+public static void main(String[] args)   
+{   
+int x = 10, y = 8, gcd = 1;  
+
+for(int i = 1;i <= x && i <= y; i++)  
+{    
+if(x%i==0 && y%i==0)  
+gcd = i;  
+}  
+System.out.printf("GCD of %d and %d is: %d", x, y, gcd);  
+}  
+}  
