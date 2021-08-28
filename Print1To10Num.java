da6653dcd2602b2a2086f1bd840192a4bbf4e5a0
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-28 21:52:37 +0530

    Add files via upload

---
+++ Print1To10Num.java
@@ -0,0 +1,18 @@
+
+
+  
+
+//Write a program to print numbers from 1 to 10.
+
+public class Print1To10Num
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Output is : ");
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
