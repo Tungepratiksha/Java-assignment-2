b5be9e79c721ea8fd082122ec6dedc36618eb4be
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-28 21:47:51 +0530

    Add files via upload

---
+++ LengthCharacterExample.java
@@ -0,0 +1,8 @@
+//Java Program to Find the Length of a String
+public class LengthCharacterExample{  
+public static void main(String args[]){  
+String s="GovernmentPoly";  
+
+System.out.println("string length is: "+s.length());
+}
+}  
