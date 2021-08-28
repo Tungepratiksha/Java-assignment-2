afda2d695429bd56a6fdde8efc03f36f6cc84eb9
Tungepratiksha <89512560+Tungepratiksha@users.noreply.github.com>
2021-08-28 15:31:30 +0530

    Add files via upload

---
+++ IncDecOperator.java
@@ -0,0 +1,13 @@
+//Increment ++ and Decrement -- Operator Overloading in Java Programming
+
+class IncDecOperator {
+    public static void main(String[] args) {
+        int var1 = 6, var2 = 2;
+
+        
+        System.out.println(++var1);
+
+       
+        System.out.println(var2++);
+    }
+}
